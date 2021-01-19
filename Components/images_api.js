c3918abd685b27edeac3a1f44c512e7cfeb99eb7
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-01-19 17:44:59 +0700

    add files

---
+++ Components/images_api.js
@@ -0,0 +1,32 @@
+
+const uploadImage = (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            let avatar = req.files.image;
+            
+            avatar.mv('./uploads/' + avatar.name);
+  
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+  }
+
+  module.exports = {
+    uploadImage,
+}
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const db = require('./Components/person_tbl_api')
-const port = 3000
+const db = require('./Components/MMF_api')
+const img = require('./Components/images_api')
+const fileUpload = require('express-fileupload');
+//start app 
+const port = process.env.PORT || 3000;
+const morgan = require('morgan');
+const _ = require('lodash');
+const cors = require('cors');
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 app.use(bodyParser.json())
 app.use(
@@ -15,11 +27,25 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
+
+
 // app.get('/birthDate', db.getBirthDate)
 // app.get('/getBirthDate/:id', db.getBirthDatebyId)
 
 //post get birthdate from DB
 app.post('/getBirthDate', db.datePost)
+//post get getCountJobTask from DB
+app.post('/getCountJobTask', db.getCountJobTask)
+//post get getListJobTask from DB
+app.post('/getListJobTask', db.getListJobTask)
+//post get getLongitude_Branch from DB
+app.post('/getLongitude_Branch', db.getLongitude_Branch)
+//post get image
+app.post('/upload-avatar', img.uploadImage);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
