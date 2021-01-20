1b097601dd27d9a777d23c5d5bebf280dfe0b466
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-01-20 14:47:52 +0700

    hcm db

--- controller/getLongitudeBranchCtrl/index.js
+++ controller/getLongitudeBranchCtrl/index.js
@@ -3,7 +3,7 @@ const pool = require('../../db')
 //Tabel : mark_location_tbl
 var controller = {
   getLongitude_Branch: function(request, response) {
-    pool.db_MMFPROD.query("select location_name, latitude,longitude from mark_location_tbl order by location_no asc", (error, results) => {
+    pool.db_MMFPROD.query("select location_name, latitude,longitude,accuracy,radius_tolerance from mark_location_tbl order by location_no asc", (error, results) => {
       if (error) {
         throw error
       }
--- controller/getURLHCM/index.js
+++ controller/getURLHCM/index.js
@@ -5,12 +5,12 @@ var controller = {
     getURL_HCM: function(request, response) {
         pool.db_HCM.query("select setting_value as url_hcm from param_hcm where setting_name ='URL API HCM'", (error, results) => {
             if (error) {
-            throw error
+                throw error
             }
             if(results.rows != ''){
-            response.status(200).json(results.rows)
+                response.status(200).json(results.rows)
             }else{
-            response.status(400).json("data tidak ditemukan")
+                response.status(400).json("data tidak ditemukan")
             }
         })
     }
