2806e4216a5b027760dc017b9f3731be394578f4
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-02-15 16:50:02 +0700

    eslint + prettier

--- controller/dateCtrl/index.js
+++ controller/dateCtrl/index.js
@@ -1,34 +1,38 @@
-const pool = require('../../db')
+const pool = require('../../db');
 
 // Tabel : person_tbl, employee_tbl
-var controller = {
-    getBirthDate: function (request, response) {
-        try {
-            const { employee_id } = request.body
-            // console.log (request.body)
+const controller = {
+  getBirthDate(request, response) {
+    try {
+      const { employee_id } = request.body;
+      // console.log (request.body)
 
-            pool.db_MMFPROD.query("SELECT b.employee_id,to_char(birth_date,'MM-DD-YYYY') as birth_Date FROM person_tbl a left join employee_tbl b on a.person_id =b.person_id WHERE b.employee_id = $1 and to_char(birth_date,'DDMM')=to_char(current_date ,'DDMM')", [employee_id], (error, results) => {
-                if (error) {
-                    throw error
-                }
-                if (results.rows != '') {
-                    response.status(200).send({
-                        status: 200,
-                        message: 'Load Data Berhasil',
-                        data: results.rows[0]
-                    });
-                } else {
-                    response.status(200).send({
-                        status: 200,
-                        message: 'Data Tidak Ditemukan',
-                        data: results.rows
-                    });
-                }
-            })
-        } catch (err) {
-            res.status(500).send(err);
+      pool.db_MMFPROD.query(
+        "SELECT b.employee_id,to_char(birth_date,'MM-DD-YYYY') as birth_Date FROM person_tbl a left join employee_tbl b on a.person_id =b.person_id WHERE b.employee_id = $1 and to_char(birth_date,'DDMM')=to_char(current_date ,'DDMM')",
+        [employee_id],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          if (results.rows !== '') {
+            response.status(200).send({
+              status: 200,
+              message: 'Load Data Berhasil',
+              data: results.rows[0],
+            });
+          } else {
+            response.status(200).send({
+              status: 200,
+              message: 'Data Tidak Ditemukan',
+              data: results.rows,
+            });
+          }
         }
+      );
+    } catch (err) {
+      response.status(500).send(err);
     }
+  },
 };
 
 module.exports = controller;
--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const db_MMFPROD = new Pool({
   user: process.env.USER_MMFPROD || 'postgres',
   host: process.env.HOST_MMFPROD || '192.168.0.111',
   database: process.env.DATABASE_MMFPROD || 'MMFPROD',
   password: process.env.PASSWORD_MMFPROD || 'Kutubuku123',
   port: process.env.PORT_MMFPROD || 5433,
-})
+});
 
 const db_HCM = new Pool({
   user: process.env.USER_HCM || 'postgres',
@@ -13,9 +13,9 @@ const db_HCM = new Pool({
   database: process.env.DATABASE_HCM || 'HCM',
   password: process.env.PASSWORD_HCM || 'Kutubuku123',
   port: process.env.PORT_HCM || 5433,
-})
+});
 
 module.exports = {
   db_MMFPROD,
-  db_HCM
-}
+  db_HCM,
+};
