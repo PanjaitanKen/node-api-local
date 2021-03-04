5754169440276ffbea53c2e4c7286b67ff5f8548
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-03-04 16:13:01 +0700

    new req

--- controller/listJobTaskCtrl/index.js
+++ controller/listJobTaskCtrl/index.js
@@ -41,8 +41,8 @@ const controller = {
           " and  a.employee_id in (select employee_id from employee_supervisor_tbl where supervisor_id = $1 and valid_to=date'9999-01-01') " +
           ' union all  ' +
           " select 'Persetujuan Perjalanan Dinas' Keterangan,initcap(c.display_name) nama,  " +
-          " case  when current_date-request_date=0 then 'Hari ini'  " +
-          " when current_date-request_date=1 then 'Kemarin'  " +
+          " case  when current_date-request_date=0 then 'Hari ini'   " +
+          " when current_date-request_date=1 then 'Kemarin'   " +
           " when current_date-request_date=2 then '2 Hari yang lalu'  " +
           " when current_date-request_date=3 then '3 Hari yang lalu'  " +
           " when current_date-request_date=4 then '4 Hari yang lalu'  " +
@@ -53,8 +53,28 @@ const controller = {
           " a.golid,'Pengajuan Perjalanan Dinas' Keterangan2 from travel_request_tbl  a  " +
           ' left join employee_tbl  b on a.employee_id = b.employee_id  ' +
           ' left join person_tbl c on b.person_id =c.person_id ' +
-          " where a.state='Submitted'  " +
-          " and  a.employee_id in (select employee_id from employee_supervisor_tbl where supervisor_id = $1 and valid_to=date'9999-01-01') ",
+          ' where a.golid in  ' +
+          ' (select ref_id from approval_structure_tbl ' +
+          " where class_name ='com.sps.travelexpense.transaction.TravelRequest' " +
+          " and approver_id= $1 and ref_id||trim(to_char(approval_sequence,'999999')) in  " +
+          ' ( ' +
+          " with x as (select ref_id,trim(to_char(min(approval_sequence),'999999'))  as seq_min " +
+          ' from approval_structure_tbl  ' +
+          " where class_name ='com.sps.travelexpense.transaction.TravelRequest' " +
+          " and state='Unapproved' and ref_id  in  " +
+          ' (select golid from travel_request_tbl trt ' +
+          " where  state in ('Submitted','Partially Approved') " +
+          ' and employee_id in (select employee_id from employee_supervisor_tbl where supervisor_id in ' +
+          " (select employee_id from employee_supervisor_tbl where supervisor_id = $1 and valid_to=date'9999-01-01') " +
+          " and valid_to=date'9999-01-01' " +
+          ' union all ' +
+          " select employee_id from employee_supervisor_tbl where supervisor_id = $1 and valid_to=date'9999-01-01') " +
+          ' )' +
+          ' group by ref_id	) ' +
+          ' select ref_id||seq_min as ref_id_min  ' +
+          '  from x ' +
+          '  ) ' +
+          " and state='Unapproved' ) ",
         [employee_id],
         (error, results) => {
           if (error) throw error;
