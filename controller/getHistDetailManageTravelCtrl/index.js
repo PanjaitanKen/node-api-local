27ea470a44455c1f9faabe6aaff867feb04f9e2d
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-03-10 15:32:47 +0700

    as

--- controller/getHistDetailManageTravelCtrl/index.js
+++ controller/getHistDetailManageTravelCtrl/index.js
@@ -101,12 +101,14 @@ const controller = {
                   response.status(200).send({
                     status: 200,
                     message: 'Load Data berhasil',
+                    validate_id: employee_id,
                     data: respData,
                   });
                 } else {
                   response.status(200).send({
                     status: 200,
                     message: 'Data Tidak Ditemukan',
+                    validate_id: employee_id,
                     data: results.rows,
                   });
                 }
@@ -116,6 +118,7 @@ const controller = {
             response.status(200).send({
               status: 200,
               message: 'Data Tidak Ditemukan',
+              validate_id: employee_id,
               data: results.rows,
             });
           }
