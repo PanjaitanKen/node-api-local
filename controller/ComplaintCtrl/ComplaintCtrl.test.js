1b145fa2d864cb83a8ea1e494a646dc85c06abf7
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-03-23 13:45:37 +0700

    jest ket

--- controller/ComplaintCtrl/ComplaintCtrl.test.js
+++ controller/ComplaintCtrl/ComplaintCtrl.test.js
@@ -2,15 +2,11 @@ const request = require('supertest');
 const app = require('../..');
 const pool = require('../../db');
 
-describe('POST /checkTokenNotif', () => {
+describe('POST /complaints', () => {
   it('responds with json', async () => {
     const res = await request(app)
-      .post('/hcm/api/checkTokenNotif')
-      .send({
-        employee_id: '0053722',
-        token_notif:
-          'eN03St2nRSWcRr8k331dmL:APA91bGcb4hEQQhOyC_aUcxwL3q-CwQ3vOEfT_B9DgnRYyujCTXIEPb9WUsnGzno-j26EDsrM2Ik11U1N_jIDnKTS7EkqJ36rm6o6-HdCL2q40_kInZivB_6Rgewc814I43wW0Q7icXf',
-      })
+      .post('/hcm/api/complaints')
+      .send({})
       .set(
         'API_KEY',
         '$2y$12$YVwznP6BkSevltSe/d64l.MEQPf/tnNR4Rax1kG.8RCKD7iC.OEJa'
