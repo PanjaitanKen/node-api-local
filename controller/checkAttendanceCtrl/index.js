53eb0006656b98c486302ea41156a2be0fac4c43
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-04-01 22:05:32 +0700

    edit query

--- controller/checkAttendanceCtrl/index.js
+++ controller/checkAttendanceCtrl/index.js
@@ -32,6 +32,7 @@ const controller = {
                     results.rows[0].time_out != '' &&
                     results.rows[0].time_out != null
                   ) {
+                    console.log('a');
                     pool.db_MMFPROD.query(
                       `SELECT count(*) FROM emp_clocking_detail_tbl 
                       where employee_id = $1 and absence_wage in 
@@ -63,6 +64,7 @@ const controller = {
                       }
                     );
                   } else {
+                    console.log('b');
                     pool.db_MMFPROD.query(
                       `select to_char(clocking_date,'HH:MI') as time_out from emp_clocking_temp_tbl where employee_id = $1
                       and in_out = '1' and TO_CHAR(clocking_date,'YYYY-MM-DD') =to_char(current_date,'YYYY-MM-DD')
@@ -70,43 +72,55 @@ const controller = {
                       [employee_id],
                       (error, results) => {
                         if (error) throw error;
-
-                        if (
-                          // eslint-disable-next-line operator-linebreak
-                          // eslint-disable-next-line eqeqeq
-                          results.rows[0].time_out != '' &&
-                          results.rows[0].time_out != null
-                        ) {
-                          pool.db_MMFPROD.query(
-                            `SELECT count(*) FROM emp_clocking_detail_tbl 
+                        console.log(results.rows);
+                        console.log(employee_id);
+                        if (results.rows != '' && results.rows != undefined) {
+                          if (
+                            // eslint-disable-next-line operator-linebreak
+                            // eslint-disable-next-line eqeqeq
+                            results.rows[0].time_out != '' &&
+                            results.rows[0].time_out != null
+                          ) {
+                            console.log('c');
+                            pool.db_MMFPROD.query(
+                              `SELECT count(*) FROM emp_clocking_detail_tbl 
                             where employee_id = $1 and absence_wage in 
                              ('CT_A_BPTIS','CT_A_NIKAH','CT_GUGUR','CT_HAJI','CT_I_LAHIR','CT_KLG_WFT',
                             'CT_LAHIR','CT_NIKAH','CT_RMH_WFT','CT_THN','CT_SKT') and clocking_date =current_date`,
-                            [employee_id],
-                            (error, results) => {
-                              if (error) throw error;
+                              [employee_id],
+                              (error, results) => {
+                                if (error) throw error;
 
-                              if (
-                                // eslint-disable-next-line operator-linebreak
-                                // eslint-disable-next-line eqeqeq
-                                results.rows[0].count == '1'
-                              ) {
-                                response.status(200).send({
-                                  status: 200,
-                                  message: 'Berhasil Clock In dan Clock Out',
-                                  validate_id: employee_id,
-                                  data: 3,
-                                });
-                              } else {
-                                response.status(200).send({
-                                  status: 200,
-                                  message: 'Berhasil Clock In dan Clock Out',
-                                  validate_id: employee_id,
-                                  data: 2,
-                                });
+                                if (
+                                  // eslint-disable-next-line operator-linebreak
+                                  // eslint-disable-next-line eqeqeq
+                                  results.rows[0].count == '1'
+                                ) {
+                                  response.status(200).send({
+                                    status: 200,
+                                    message: 'Berhasil Clock In dan Clock Out',
+                                    validate_id: employee_id,
+                                    data: 3,
+                                  });
+                                } else {
+                                  response.status(200).send({
+                                    status: 200,
+                                    message: 'Berhasil Clock In dan Clock Out',
+                                    validate_id: employee_id,
+                                    data: 2,
+                                  });
+                                }
                               }
-                            }
-                          );
+                            );
+                          } else {
+                            console.log('d');
+                            response.status(200).send({
+                              status: 200,
+                              message: 'Berhasil Clock In dan Belum Clock Out',
+                              validate_id: employee_id,
+                              data: 1,
+                            });
+                          }
                         } else {
                           response.status(200).send({
                             status: 200,
@@ -195,45 +209,57 @@ const controller = {
                             [employee_id],
                             (error, results) => {
                               if (error) throw error;
-
                               if (
-                                // eslint-disable-next-line operator-linebreak
-                                // eslint-disable-next-line eqeqeq
-                                results.rows[0].time_out != '' &&
-                                results.rows[0].time_out != null
+                                results.rows != '' &&
+                                results.rows != undefined
                               ) {
-                                pool.db_MMFPROD.query(
-                                  `SELECT count(*) FROM emp_clocking_detail_tbl 
+                                if (
+                                  // eslint-disable-next-line operator-linebreak
+                                  // eslint-disable-next-line eqeqeq
+                                  results.rows[0].time_out != '' &&
+                                  results.rows[0].time_out != null
+                                ) {
+                                  pool.db_MMFPROD.query(
+                                    `SELECT count(*) FROM emp_clocking_detail_tbl 
                                   where employee_id = $1 and absence_wage in 
                                    ('CT_A_BPTIS','CT_A_NIKAH','CT_GUGUR','CT_HAJI','CT_I_LAHIR','CT_KLG_WFT',
                                   'CT_LAHIR','CT_NIKAH','CT_RMH_WFT','CT_THN','CT_SKT') and clocking_date =current_date`,
-                                  [employee_id],
-                                  (error, results) => {
-                                    if (error) throw error;
+                                    [employee_id],
+                                    (error, results) => {
+                                      if (error) throw error;
 
-                                    if (
-                                      // eslint-disable-next-line operator-linebreak
-                                      // eslint-disable-next-line eqeqeq
-                                      results.rows[0].count == '1'
-                                    ) {
-                                      response.status(200).send({
-                                        status: 200,
-                                        message:
-                                          'Berhasil Clock In dan Clock Out',
-                                        validate_id: employee_id,
-                                        data: 3,
-                                      });
-                                    } else {
-                                      response.status(200).send({
-                                        status: 200,
-                                        message:
-                                          'Berhasil Clock In dan Clock Out',
-                                        validate_id: employee_id,
-                                        data: 2,
-                                      });
+                                      if (
+                                        // eslint-disable-next-line operator-linebreak
+                                        // eslint-disable-next-line eqeqeq
+                                        results.rows[0].count == '1'
+                                      ) {
+                                        response.status(200).send({
+                                          status: 200,
+                                          message:
+                                            'Berhasil Clock In dan Clock Out',
+                                          validate_id: employee_id,
+                                          data: 3,
+                                        });
+                                      } else {
+                                        response.status(200).send({
+                                          status: 200,
+                                          message:
+                                            'Berhasil Clock In dan Clock Out',
+                                          validate_id: employee_id,
+                                          data: 2,
+                                        });
+                                      }
                                     }
-                                  }
-                                );
+                                  );
+                                } else {
+                                  response.status(200).send({
+                                    status: 200,
+                                    message:
+                                      'Berhasil Clock In dan Belum Clock Out',
+                                    validate_id: employee_id,
+                                    data: 1,
+                                  });
+                                }
                               } else {
                                 response.status(200).send({
                                   status: 200,
