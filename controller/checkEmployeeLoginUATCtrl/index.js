57e14730fa2f1a0c61d61c6f63f0e36263833d71
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-04-07 10:00:24 +0700

    cek data table berisi atau tidak

--- controller/checkEmployeeLoginUATCtrl/index.js
+++ controller/checkEmployeeLoginUATCtrl/index.js
@@ -7,26 +7,44 @@ const controller = {
       const { employee_id } = request.body;
 
       pool.db_HCM.query(
-        ' select count(*) jumlah from mas_karyawan_uat where employee_code = $1',
-        [employee_id],
+        ' select count(*) jumlah from mas_karyawan_uat',
         (error, results) => {
           if (error) {
             throw error;
           }
           // eslint-disable-next-line eqeqeq
-          if (results.rows != '') {
+          if (results.rows[0].jumlah != 0) {
+            pool.db_HCM.query(
+              ' select count(*) jumlah from mas_karyawan_uat where employee_code = $1',
+              [employee_id],
+              (error, results) => {
+                if (error) {
+                  throw error;
+                }
+                // eslint-disable-next-line eqeqeq
+                if (results.rows != '') {
+                  response.status(200).send({
+                    status: 200,
+                    message: 'Load Data berhasil',
+                    validate_id: employee_id,
+                    data: results.rows[0],
+                  });
+                } else {
+                  response.status(200).send({
+                    status: 200,
+                    message: 'Data Tidak Ditemukan',
+                    validate_id: employee_id,
+                    data: results.rows,
+                  });
+                }
+              }
+            );
+          } else {
             response.status(200).send({
               status: 200,
               message: 'Load Data berhasil',
               validate_id: employee_id,
-              data: results.rows[0],
-            });
-          } else {
-            response.status(200).send({
-              status: 200,
-              message: 'Data Tidak Ditemukan',
-              validate_id: employee_id,
-              data: results.rows,
+              data: '',
             });
           }
         }
