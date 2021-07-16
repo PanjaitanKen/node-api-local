847b23179a33d9f7831e28e6089f8734b8ab38ab
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-07-16 14:21:59 +0700

    edit query

--- controller/getHistDetailManageRevAbsenceCtrl/index.js
+++ controller/getHistDetailManageRevAbsenceCtrl/index.js
@@ -61,8 +61,8 @@ const controller = {
                when to_char(b.status_Date ,'MM')='10' then 'Okt'
                when to_char(b.status_Date ,'MM')='11' then 'Nov'
                when to_char(b.status_Date ,'MM')='12' then 'Des' end ||' '||to_char(b.status_Date ,'YYYY') tgl_status, 
-               reg_time_in as jam_masuk_terdaftar , reg_time_out as jam_keluar_terdaftar,  
-               rev_time_in as jam_masuk_diperbaiki , rev_time_out as jam_keluar_diperbaiki,
+               TO_CHAR(reg_time_in,'YYYY-MM-DD HH24:MI:SS') as jam_masuk_terdaftar ,TO_CHAR(reg_time_out,'YYYY-MM-DD HH24:MI:SS') as jam_keluar_terdaftar,  
+               TO_CHAR(rev_time_in,'YYYY-MM-DD HH24:MI:SS') as jam_masuk_diperbaiki, TO_CHAR(rev_time_out,'YYYY-MM-DD HH24:MI:SS') as jam_keluar_diperbaiki,
                a.reason as alasan,  a.rev_absence_id  as golid,
                case when a.state='Approved' then 'Disetujui'
                   when a.state='Rejected' then 'Ditolak'
