92ccf4016c12f435c011061ab2e553adf65b21e2
PanjaitanKen <ken.bagasnandito@mandalafinance.com>
2021-09-06 15:57:47 +0700

    async part III

--- controller/notificationManagementCtrl/index.js
+++ controller/notificationManagementCtrl/index.js
@@ -6,7 +6,7 @@ const { validationResult } = require('express-validator');
 const pool = require('../../db');
 
 const controller = {
-  insert(request, response) {
+  async insert(request, response) {
     const errors = validationResult(request);
     if (!errors.isEmpty()) return response.status(422).send(errors);
 
@@ -15,18 +15,18 @@ const controller = {
     } = request;
 
     try {
-      pool.db_HCM.query(
-        'select * from temp_notif where employee_id = $1 and golid =$2 and jenis =$3',
-        [employee_id, golid, jenis],
-        (error, results) => {
-          if (error) throw error;
+      const query =
+        'select * from temp_notif where employee_id = $1 and golid =$2 and jenis =$3';
 
-          if (results.rows != '') {
+      await pool.db_HCM
+        .query(query, [employee_id, golid, jenis])
+        .then(({ rows }) => {
+          if (rows != '') {
             response.status(200).send({
               status: 200,
               message: 'Data already exist',
               validate_id: employee_id,
-              data: results.rows,
+              data: rows,
             });
           } else {
             pool.db_HCM.query(
@@ -44,56 +44,57 @@ const controller = {
               }
             );
           }
-        }
-      );
+        })
+        .catch((error) => {
+          throw error;
+        });
     } catch (error) {
       response.status(500).send(error);
     }
   },
-  countTempNotif(request, response) {
+  async countTempNotif(request, response) {
     const errors = validationResult(request);
     if (!errors.isEmpty()) return response.status(422).send(errors);
 
     const { employee_id } = request.body;
 
     try {
-      pool.db_HCM.query(
-        `select count(*) from temp_notif where
+      const query = `select count(*) from temp_notif where
         employee_id =$1 and sudah_baca is null
-        and approved_date between (current_date -interval '1 days' * 20) and now()`,
-        [employee_id],
-        (error, results) => {
-          if (error) throw error;
-
-          if (results.rows != '') {
+        and approved_date between (current_date -interval '1 days' * 20) and now()`;
+      await pool.db_HCM
+        .query(query, [employee_id])
+        .then(({ rows }) => {
+          if (rows != '') {
             response.status(200).send({
               status: 200,
               message: 'Load Data berhasil',
               validate_id: employee_id,
-              data: results.rows[0],
+              data: rows[0],
             });
           } else {
             response.status(200).send({
               status: 200,
               message: 'Data Tidak Ditemukan',
               validate_id: employee_id,
-              data: results.rows,
+              data: '',
             });
           }
-        }
-      );
+        })
+        .catch((error) => {
+          throw error;
+        });
     } catch (error) {
       response.status(500).send(error);
     }
   },
-  listTempNotif(request, response) {
+  async listTempNotif(request, response) {
     const errors = validationResult(request);
     if (!errors.isEmpty()) return response.status(422).send(errors);
 
     const { employee_id } = request.body;
     try {
-      pool.db_HCM.query(
-        `select a.employee_id, a.jenis, a.ket1, a.ket2, a.nobukti, a.golid, a.approved_Date, a.sudah_baca,
+      const query = `select a.employee_id, a.jenis, a.ket1, a.ket2, a.nobukti, a.golid, a.approved_Date, a.sudah_baca,
         case  when current_date-a.approved_date=0 then 'Hari ini'
                             when current_date-a.approved_date=1 then 'Kemarin'
                             when current_date-a.approved_date=2 then '2 Hari yang lalu'
@@ -107,17 +108,16 @@ const controller = {
         from temp_notif a 
         where
         employee_id =$1 
-        and approved_date between (current_date -interval '1 days' * 20) and now() order by a.nourut_notif desc `,
-        [employee_id],
-        (error, results) => {
-          if (error) throw error;
-
-          if (results.rows != '') {
+        and approved_date between (current_date -interval '1 days' * 20) and now() order by a.nourut_notif desc `;
+      await pool.db_HCM
+        .query(query, [employee_id])
+        .then(({ rows }) => {
+          if (rows != '') {
             response.status(200).send({
               status: 200,
               message: 'Load Data berhasil',
               validate_id: employee_id,
-              data: results.rows,
+              data: rows,
             });
           } else {
             response.status(200).send({
@@ -127,58 +127,57 @@ const controller = {
               data: '',
             });
           }
-        }
-      );
+        })
+        .catch((error) => {
+          throw error;
+        });
     } catch (error) {
       response.status(500).send(error);
     }
   },
-  updateTempNotif(request, response) {
+  async updateTempNotif(request, response) {
     const errors = validationResult(request);
     if (!errors.isEmpty()) return response.status(422).send(errors);
 
     const { employee_id, golid } = request.body;
 
     try {
-      pool.db_HCM.query(
-        'select sudah_baca from temp_notif where employee_id = $1 and golid = $2',
-        [employee_id, golid],
-        (error, results) => {
-          if (error) throw error;
-          if (results.rows != '') {
-            if (results.rows[0].sudah_baca == null) {
-              pool.db_HCM.query(
-                `update temp_notif set sudah_baca=current_date where employee_id =$1
+      const query =
+        'select sudah_baca from temp_notif where employee_id = $1 and golid = $2';
+      await pool.db_HCM.query(query, [employee_id, golid]).then(({ rows }) => {
+        if (rows != '') {
+          if (rows[0].sudah_baca == null) {
+            pool.db_HCM.query(
+              `update temp_notif set sudah_baca=current_date where employee_id =$1
               and sudah_baca is null and golid=$2 `,
-                [employee_id, golid],
-                (error) => {
-                  if (error) throw error;
+              [employee_id, golid],
+              (error) => {
+                if (error) throw error;
 
-                  response.status(202).send({
-                    status: 'SUCCESS UPDATE',
-                    message: 'Notification succes update to table',
-                    data: '',
-                  });
-                }
-              );
-            } else {
-              response.status(200).send({
-                status: 'UPDATE FAILED',
-                message: 'Notification data already set',
-                data: '',
-              });
-            }
+                response.status(202).send({
+                  status: 'SUCCESS UPDATE',
+                  message: 'Notification succes update to table',
+                  data: '',
+                });
+              }
+            );
           } else {
             response.status(200).send({
-              status: 200,
-              message: 'Data Tidak Ditemukan',
-              validate_id: employee_id,
-              golid_id: golid,
+              status: 'UPDATE FAILED',
+              message: 'Notification data already set',
               data: '',
             });
           }
+        } else {
+          response.status(200).send({
+            status: 200,
+            message: 'Data Tidak Ditemukan',
+            validate_id: employee_id,
+            golid_id: golid,
+            data: '',
+          });
         }
-      );
+      });
     } catch (error) {
       response.status(500).send(error);
     }
